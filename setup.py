2a83a0b6a2653f701b21de0a326c6e581215fc2b
shrishtiroy <136747691+shrishtiroy@users.noreply.github.com>
2025-05-20 16:56:37 -0700

    requirements updated

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_USER_NAME = "shrishtiroy"
+SRC_REPO = "text-summarizer"
+AUTHOR_EMAIL = "shrishtir06@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for text summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={"bug trackers": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
