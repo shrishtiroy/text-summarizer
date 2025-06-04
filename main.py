8f60fb3d65f4162305c6309be0ed46bd816b2b69
shrishtiroy <136747691+shrishtiroy@users.noreply.github.com>
2025-06-04 09:17:53 -0700

    added stage 2 data validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 if __name__ == "__main__":
@@ -7,6 +8,14 @@ if __name__ == "__main__":
         pipeline = DataIngestionTrainingPipeline()
         pipeline.main()
         logger.info("Stage 01: Data Ingestion completed")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    try:
+        logger.info("Stage 02: Data Validation started")
+        pipeline = DataValidationTrainingPipeline()
+        pipeline.main()
+        logger.info("Stage 02: Data Validation completed")
     except Exception as e:
         logger.exception(e)
         raise e
